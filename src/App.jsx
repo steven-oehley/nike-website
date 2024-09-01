a39b2d4fd011c9ad70a4b4df76f736b374c361bb
Steve <steve.gitprofile@outlook.com>
2024-09-01 19:18:20 +0200

    setup app structure with placeholders

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+export const App = () => {
+  return (
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+      <section className="padding">PopularProducts</section>
+      <section className="padding">SuperQuality</section>
+      <section className="padding-x py-10">Services</section>
+      <section className="padding">Special Offers</section>
+      <section className="padding bg-pale-blue">Customer Reviews</section>
+      <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+      <section className="bg-black padding-x padding-t pb-8">Footer</section>
+    </main>
+  );
+};
+
+export default App;
