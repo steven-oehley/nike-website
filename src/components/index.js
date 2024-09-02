860f1a976c4e770b992aab4c2b7fceaaf76f21c9
Steve <steve.gitprofile@outlook.com>
2024-09-02 21:27:56 +0200

    added services section

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import Button from "./Button";
 import Nav from "./Nav";
 import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
 import ShoeCard from "./ShoeCard";
 
-export { Button, Nav, PopularProductCard, ShoeCard };
+export { Button, Nav, PopularProductCard, ServiceCard, ShoeCard };
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,4 +1,13 @@
+import { services } from "../constants";
+import { ServiceCard } from "../components";
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="max-contain flex flex-wrap justify-center gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
+  );
 };
 export default Services;
