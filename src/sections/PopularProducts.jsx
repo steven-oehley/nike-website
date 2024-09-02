fe451a958e1abbf423c8a5ddd806214ba8d7eeeb
Steve <steve.gitprofile@outlook.com>
2024-09-02 20:59:18 +0200

    added PopularProducts section

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,4 +1,24 @@
+import { products } from "../constants";
+import { PopularProductCard } from "../components";
+
 const PopularProducts = () => {
-  return <div>PopularProducts</div>;
+  return (
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="font-palanquin text-4xl font-bold">
+          Our <span className="text-coral-red">Popular</span> Products
+        </h2>
+        <p className="mt-2 font-montserrat text-slate-gray lg:max-w-lg">
+          Experience top notch quality and comfort with our best selling
+          selection. Discover a world of comfort, design and style.
+        </p>
+      </div>
+      <div className="mt-16 grid grid-cols-1 gap-14 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {products.map((product) => (
+          <PopularProductCard key={product.name} {...product} />
+        ))}
+      </div>
+    </section>
+  );
 };
 export default PopularProducts;
