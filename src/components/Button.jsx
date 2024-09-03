dcd16a7f462078bad9356269263300acefc879e0
Steve <steve.gitprofile@outlook.com>
2024-09-03 09:44:39 +0200

    added special offers section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,14 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+}) => {
   return (
-    <button className="font-lg flex items-center justify-center gap-2 rounded-full border border-coral-red bg-coral-red px-7 py-4 font-montserrat text-white">
+    <button
+      className={`font-lg flex items-center justify-center gap-2 rounded-full border ${backgroundColor ? backgroundColor : "bg-coral-red"} ${borderColor ? borderColor : "border-coral-red"} px-7 py-4 font-montserrat ${textColor ? textColor : "text-white"}`}
+    >
       {label}
       {iconURL && (
         <img
--- src/sections/SpecialOffers.jsx
+++ src/sections/SpecialOffers.jsx
@@ -1,4 +1,44 @@
+import { Button } from "../components";
+import { offer } from "../assets/images";
+import { arrowRight } from "../assets/icons";
+
 const SpecialOffers = () => {
-  return <div>SpecialOffers</div>;
+  return (
+    <section className="justify-wrap max-container max-container flex items-center gap-10 max-xl:flex-col-reverse">
+      <div className="flex-1">
+        <img
+          src={offer}
+          alt="nike 30% off offer"
+          width={773}
+          height={687}
+          className="w-full object-contain"
+        />
+      </div>
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl font-bold capitalize lg:max-w-lg">
+          Special
+          <span className="text-coral-red"> Offer</span>
+        </h2>
+        <p className="info-text mt-4 max-w-lg">
+          Discover the perfect blend of quality and design with our exceptional
+          footwear. Every pair is crafted to offer you superior comfort, style,
+          and durability.
+        </p>
+        <p className="info-text mt-6 lg:max-w-lg">
+          Have you been waiting for a sign? This is it! Take 30% off our best
+          styles now and step into unparalleled savings and style.
+        </p>
+        <div className="mt-11 flex flex-wrap gap-4">
+          <Button label="Shop Now" iconURL={arrowRight} />
+          <Button
+            label="Learn More"
+            backgroundColor="bg-white"
+            borderColor="border-slate-gray"
+            textColor="text-slate-gray"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 export default SpecialOffers;
